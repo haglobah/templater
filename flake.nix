5cec5c868f0fe450329f0464370e25d4178e41b9
Beat Hagenlocher <bah@posteo.de>
2025-09-10 00:53:19 +0200

    Indent

--- flake.nix
+++ flake.nix
@@ -16,56 +16,56 @@
             p.colorama
           ]);
         in
-        {
-          packages.rust = pkgs.rustPlatform.buildRustPackage rec {
-            pname = "templater";
-            meta.mainProgram = "templater";
-            version = "0.2";
-            cargoLock.lockFile = ./Cargo.lock;
-            src = ./.;
-          };
+          {
+            packages.rust = pkgs.rustPlatform.buildRustPackage rec {
+              pname = "templater";
+              meta.mainProgram = "templater";
+              version = "0.2";
+              cargoLock.lockFile = ./Cargo.lock;
+              src = ./.;
+            };
 
-          packages.default = pkgs.writeShellApplication {
-            name = "templater";
-            runtimeInputs = [ self'.packages.rust ];
-            text = ''
+            packages.default = pkgs.writeShellApplication {
+              name = "templater";
+              runtimeInputs = [ self'.packages.rust ];
+              text = ''
               ${self'.packages.rust}/bin/templater --from ${self}/templates "$@"
             '';
+            };
+
+            packages.python = pkgs.writeShellApplication {
+              name = "templater";
+              runtimeInputs = [ mypython ];
+              text = ''
+                exec ${mypython}/bin/python ${self}/templater.py --from ${self}/templates "$@"
+              '';
+            };
+
+            apps.default = {
+              type = "app";
+              program = "${pkgs.lib.getExe self'.packages.default}";
+            };
+
+            devShells.default = pkgs.mkShell {
+              buildInputs = [
+                mypython
+                pkgs.rustc
+                pkgs.cargo
+                pkgs.rust-analyzer
+                pkgs.rustfmt
+              ];
+            };
+
+            checks.python-templater-tests = pkgs.runCommand "templater-tests" {
+              buildInputs = [ mypython ];
+              # Pass the test + script files into the build environment
+              src = self;
+            } ''
+                cp $src/test_templater.py .
+                cp $src/templater.py .
+                ${mypython}/bin/python test_templater.py
+                touch $out
+              '';
           };
-
-        packages.python = pkgs.writeShellApplication {
-          name = "templater";
-          runtimeInputs = [ mypython ];
-          text = ''
-            exec ${mypython}/bin/python ${self}/templater.py --from ${self}/templates "$@"
-          '';
-        };
-
-        apps.default = {
-          type = "app";
-          program = "${pkgs.lib.getExe self'.packages.default}";
-        };
-
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            mypython
-            pkgs.rustc
-            pkgs.cargo
-            pkgs.rust-analyzer
-            pkgs.rustfmt
-          ];
-        };
-
-        checks.python-templater-tests = pkgs.runCommand "templater-tests" {
-          buildInputs = [ mypython ];
-          # Pass the test + script files into the build environment
-          src = self;
-        } ''
-          cp $src/test_templater.py .
-          cp $src/templater.py .
-          ${mypython}/bin/python test_templater.py
-          touch $out
-        '';
-      };
     };
 }
